118441c4677850bd30e11e2d5b3425f0b574d7cb
Omar <cahocyjei@gmail.com>
2022-01-31 17:18:13 -0500

    Completando la estructura del proyecto, pages,containers ect..

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,13 @@ import Login from '../containers/Login';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound' 
 import RecoveryPassword from '../containers/RecoveryPassword';
+import NewPassword from '../pages/NewPassword';
+import CreateAccount from '../pages/CreateAccount';
+import Orders from '../pages/Orders';
+import MyAccount from '../pages/MyAccount';
+import Checkout from '../pages/Checkout';
+import SendEmail from '../pages/SendEmail';
+
 import '../styles/global.css';
 
 const App = () => {
@@ -15,6 +22,12 @@ const App = () => {
                         <Route  path = "/" element={<Home />} />
                         <Route  path = "/login" element={<Login />} />
                         <Route  path = "/Recovery-Password" element={<RecoveryPassword />} />
+                        <Route  path = "/New-Password" element={<NewPassword />} />
+                        <Route  path = "/signUp" element={<CreateAccount />} />
+                        <Route path = "/Orders" element= {<Orders />} />
+                        <Route path = "/My-Account" element= {<MyAccount />} />
+                        <Route path = "/Checkout" element= {<Checkout />} />
+                        <Route path = "/Send-Email" element= {<SendEmail />} />
                         <Route path= "*" element={<NotFound />} />  
                     </Routes>
                 </Layout>
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js',
+        publicPath:'/'
+    },
+    mode:'development',
+    resolve:{
+        extensions:['.js','.jsx'],
+    },
+    module:{   
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+              
+            { 
+                test: /\.html$/,
+                use:[
+                    {
+                        loader:'html-loader',
+                    }
+                ]
+            },
+            {
+                test:/\.(css|scss|sass)$/i,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                      use: [
+                         {
+                        loader: 'file-loader',
+                           options: {
+                                 name: 'assets/[hash]-[name].[ext]',
+                               },
+                         },
+                    ],
+            },
+
+        ],
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'[name].css'
+        })
+    ],
+    devServer: {
+        historyApiFallback:true,
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3005,
+      },
+}
