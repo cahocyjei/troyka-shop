1c3faa2f9be9f32f5760e044d94d30c900620b53
Omar Cabarcas <cahocyjei@gmail.com>
2022-03-04 15:37:47 -0500

    Refactorizacion Código

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -3,17 +3,11 @@ import Header from '../components/Header';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
 import useGetProducts from '../hooks/useGetProducts';
-import AuthLogin from '../hooks/authLogin';
 
 
-
-const API = 'https://api.escuelajs.co/api/v1/products';
-	const AUTH = 'https://troykamarket.herokuapp.com/troyka/api/auth/authenticate';
+	const API = 'https://api.escuelajs.co/api/v1/products';
 const ProductList = () => {
 	const products = useGetProducts(API);
-	const login = AuthLogin(AUTH,{username:"omar",password:"troyka"});
-	console.log(login);
-	
 	return (
 		<section className="main-container">
 			<Header />
